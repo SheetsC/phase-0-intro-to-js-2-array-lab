4b7702e7b17fe279d7c00c18b6d63e93dc05f6c8
Connor Sheets <connorsheets32@gmail.com>
2022-12-22 09:32:21 -0600

    commit tree 4087f99c8309c2d2a7821f92ecbf116d4faa241d

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function removeLastCat(){
+    return copyOfCats.pop();
+}
+function removeFirstCat(){
+
+}
+function destructivelyAppendCat(name){
+     cats.push(name);
+    }
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const copyOfCats = [...cats];
+    copyOfCats.push(name);
+    return copyOfCats
+    
+}
+function prependCat(name){
+    const copyOfCats = [...cats];
+    copyOfCats.unshift(name);
+    return copyOfCats
+}
+function removeLastCat(){
+    const copyOfCats = [...cats];
+    copyOfCats.pop();
+    return copyOfCats;
+ }
+ function removeFirstCat(){
+     const copyOfCats = [...cats];
+     copyOfCats.shift();
+     return copyOfCats;
+}
